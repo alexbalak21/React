6564694ecb4065e9dbab74249fa1002efa29d970
alexbalak21 <alex.balak@outlook.com>
2023-09-12 23:10:10 +0200

    Working

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,7 +1,11 @@
 function ListItem(name = "", key = 0) {
-
-    return name && <li key={key} className={key === selectedItem ? 'list-group-item : active' : 'list-group-item'}>{name}</li>
-
+    return (
+        name && (
+            <li key={key} className="list-group-item">
+                {name}
+            </li>
+        )
+    )
 }
 
 export default ListItem
