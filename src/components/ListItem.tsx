6bbbdf7469c998dd5cb52b04fe6ba3001864669c
alexbalak21 <alex.balak@outlook.com>
2023-09-25 11:20:29 +0200

    handleClick added to te li

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,7 +1,21 @@
+import { MouseEvent } from "react"
+
+let selectedIndex = 0
+
 function ListItem(name = "", key = 0) {
+    const handleClick = (event: MouseEvent) => console.log(event)
+
     return (
         name && (
-            <li key={key} className="list-group-item">
+            <li
+                key={key}
+                className={
+                    selectedIndex === key
+                        ? "list-group-item text-center active"
+                        : "list-group-item text-center"
+                }
+                onClick={handleClick}
+            >
                 {name}
             </li>
         )
