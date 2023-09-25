8fcb79681a1c40c574917755d7a9a973d15bb78a
alexbalak21 <alex.balak@outlook.com>
2023-09-25 14:05:08 +0200

    Component created with inner text props.

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,11 @@
-import ListGroup from "./components/ListGroup"
+import Alert from "./components/Alert"
 
 function App() {
-    const items = [
-        "Mariehamn",
-        "Tirana",
-        "Andorra la Vella",
-        "Vienna",
-        "Minsk",
-        "Brussels",
-        "Sarajevo",
-        "Sofia",
-        "Zagreb",
-        "Prague",
-        "Copenhagen",
-        "Tallinn",
-        "Tórshavn",
-        "Helsinki",
-        "Paris",
-        "Berlin",
-        "Gibraltar",
-        "Athens",
-        "Saint Peter Port",
-        "Budapest",
-        "Reykjavík",
-        "Dublin",
-        "Douglas",
-        "Rome",
-        "Saint Helier",
-        "Pristina",
-        "Riga",
-        "Vaduz",
-        "Vilnius",
-        "Luxembourg",
-        "Valletta",
-        "Chisinau",
-        "Monaco",
-        "Podgorica",
-        "Amsterdam",
-        "Skopje",
-        "Oslo",
-        "Warsaw",
-        "Lisbon",
-        "Bucharest",
-        "Moscow",
-        "San Marino",
-        "Belgrade",
-        "Bratislava",
-        "Ljubljana",
-        "Madrid",
-        "Longyearbyen",
-        "Stockholm",
-        "Bern",
-        "Kyiv",
-        "London",
-        "Vatican City",
-    ]
-    const handleSelectItem = (item: string) => console.log(item)
-
     return (
         <div>
-            <ListGroup
-                items={items}
-                heading="Capitals"
-                onSelectItem={handleSelectItem}
-            ></ListGroup>
+            <Alert>
+                <span>Learning REACT</span>
+            </Alert>
         </div>
     )
 }
