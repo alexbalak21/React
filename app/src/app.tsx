66fb1d4467993d7f68eebbe3e27523dc01b6d1a2
alexblack24 <alex.black@mail.com>
2024-05-07 22:54:39 +0200

    using props

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,9 +1,41 @@
 import ListGroup from "./components/ListGroup"
 
 export function App() {
+    let cities = [
+        "New York",
+        "Los Angeles",
+        "Chicago",
+        "Houston",
+        "Phoenix",
+        "Philadelphia",
+        "San Antonio",
+        "San Diego",
+        "Dallas",
+        "San Jose",
+        "London",
+        "Paris",
+        "Berlin",
+        "Rome",
+        "Madrid",
+        "Tokyo",
+        "Seoul",
+        "Shanghai",
+        "Beijing",
+        "Mumbai",
+        "São Paulo",
+        "Buenos Aires",
+        "Mexico City",
+        "Toronto",
+        "Sydney",
+        "Melbourne",
+        "Cape Town",
+        "Johannesburg",
+        "Nairobi",
+        "Cairo",
+    ]
     return (
         <div>
-            <ListGroup />
+            <ListGroup cities={cities} heading="Cities" />
         </div>
     )
 }
--- app/src/components/ListGroup.tsx
+++ app/src/components/ListGroup.tsx
@@ -1,46 +1,17 @@
 import {useState} from "preact/hooks"
+interface Props {
+    cities: string[]
+    heading: string
+}
 
-function ListGroup() {
-    let cities = [
-        "New York",
-        "Los Angeles",
-        "Chicago",
-        "Houston",
-        "Phoenix",
-        "Philadelphia",
-        "San Antonio",
-        "San Diego",
-        "Dallas",
-        "San Jose",
-        "London",
-        "Paris",
-        "Berlin",
-        "Rome",
-        "Madrid",
-        "Tokyo",
-        "Seoul",
-        "Shanghai",
-        "Beijing",
-        "Mumbai",
-        "São Paulo",
-        "Buenos Aires",
-        "Mexico City",
-        "Toronto",
-        "Sydney",
-        "Melbourne",
-        "Cape Town",
-        "Johannesburg",
-        "Nairobi",
-        "Cairo",
-    ]
+function ListGroup({cities, heading}: Props) {
     const messageIfEmpty = cities.length === 0 && <p>No Item found</p>
 
     const [selectedCity, setSelectedCity] = useState(-1)
-    const handleClick = (event: MouseEvent) => console.log(event)
 
     return (
         <>
-            <h1>List Group</h1>
+            <h1>{heading}</h1>
             {messageIfEmpty}
             <ul className="list-group">
                 {cities.map((city, id) => (
