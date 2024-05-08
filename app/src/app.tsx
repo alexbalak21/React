dde3fe77f54298cf7a6263e4989efee99314d3bc
alexblack24 <alex.black@mail.com>
2024-05-08 23:48:15 +0200

    Like component working as intended

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,62 +1,9 @@
-import {useState} from "preact/hooks"
-
-import ListGroup from "./components/ListGroup"
+import Like from "./components/Like"
 
 export function App() {
-    const cities = [
-        "New York",
-        "Los Angeles",
-        "Chicago",
-        "Houston",
-        "Phoenix",
-        "Philadelphia",
-        "San Antonio",
-        "San Diego",
-        "Dallas",
-        "San Jose",
-        "San Francisco",
-        "Seattle",
-        "Denver",
-        "Boston",
-        "Miami",
-        "Atlanta",
-        "London",
-        "Paris",
-        "Berlin",
-        "Rome",
-        "Madrid",
-        "Lisbon",
-        "Amsterdam",
-        "Brussels",
-        "Prague",
-        "Vienna",
-        "Tokyo",
-        "Seoul",
-        "Shanghai",
-        "Beijing",
-        "Bangkok",
-        "Mumbai",
-        "Sydney",
-        "Melbourne",
-        "Cape Town",
-        "Johannesburg",
-        "Cairo",
-        "Buenos Aires",
-        "São Paulo",
-        "Mexico City",
-        "Toronto",
-        "Vancouver",
-        "Montreal",
-        "Auckland",
-        "Dubai",
-        "Moscow",
-        "Istanbul",
-        "Singapore",
-        "Hong Kong",
-    ]
     return (
         <div>
-            <ListGroup heading="Cities" onSelectCity={(id) => console.log(id)} cities={cities}></ListGroup>
+            <Like onClick={() => console.log("Like")} />
         </div>
     )
 }
