5743f10e4926045bc4e49d408861b74d9031b1ac
alexblack24 <alex.black@mail.com>
2024-05-09 16:52:53 +0200

    Immer library installed and used for useState

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,24 +1,32 @@
 import {useState} from "preact/hooks"
+import {produce} from "immer"
 
 export function App() {
-    let text = ""
-    const [pizza, setPizza] = useState({
-        name: "Margarita",
-        toppings: ["Mushroom"],
-    })
+    const [bugs, setBugs] = useState([
+        {id: 1, title: "Bug 1", fixed: true},
+        {id: 2, title: "Bug 2", fixed: false},
+    ])
+
     const handleClick = () => {
-        setPizza({name: pizza.name, toppings: [...pizza.toppings, text]})
+        setBugs(
+            produce((draft) => {
+                const bug = draft.find((bug) => bug.id === 2)
+                if (bug) bug.fixed = true
+            })
+        )
     }
+
+    const handleDisplay = () => console.log(bugs)
 
     return (
         <div>
-            <ul>
-                {pizza.toppings.map((value) => (
-                    <li>{value}</li>
-                ))}
-            </ul>
-            <input type="text" value={text} />
-            <button onClick={handleClick}>Add Toping</button>
+            {bugs.map((bug) => (
+                <p key={bug.id}>
+                    {bug.title} {bug.fixed ? "Fixed" : "Not Fixed"}
+                </p>
+            ))}
+            <button onClick={handleClick}>Fix</button> <br />
+            <button onClick={handleDisplay}>Display</button>
         </div>
     )
 }
