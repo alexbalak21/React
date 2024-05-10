25dbb59ab33502fb8938e348057fd543820e4709
alexblack24 <alex.black@mail.com>
2024-05-10 11:05:47 +0200

    form input values collected using "useRef"

--- app/src/components/Form.tsx
+++ app/src/components/Form.tsx
@@ -1,18 +1,32 @@
-import { FormEvent } from "react"
+import { FormEvent, useRef } from "react"
 
 function Form() {
-  function onSubmit(event: FormEvent) {
+  const person = { name: "", age: 0 }
+  const onSubmit = (event: FormEvent) => {
     event.preventDefault()
-    console.log("Submit")
+    if (nameRef.current !== null) person.name = nameRef.current.value
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value)
+    console.log(person)
   }
 
+  const nameRef = useRef<HTMLInputElement>(null)
+  const ageRef = useRef<HTMLInputElement>(null)
+
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="name">Name</label>
-      <br />
-      <input id="name" type="text" />
-      <br />
-      <button>Submit</button>
+    <form onSubmit={onSubmit} className="my-5">
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input ref={nameRef} id="name" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input ref={ageRef} id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary">Submit</button>
     </form>
   )
 }
--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, "dist")))
 app.get("/", function (req, res) {
-  res.send(process.env.PORT)
+  res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 
-app.listen(process.env.PORT)
+app.listen(3000)
