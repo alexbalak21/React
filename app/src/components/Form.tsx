4b7e7ff36e4aba6aade7c0119dabfaabfe285dae
alexblack24 <alex.black@mail.com>
2024-05-10 18:03:30 +0200

    Working custom validation

--- app/src/components/Form.tsx
+++ app/src/components/Form.tsx
@@ -1,37 +1,38 @@
-import { FormEvent, useState } from "react"
-import { FieldValues, useForm } from "react-hook-form"
+import {FieldValues, useForm} from "react-hook-form";
+import {z} from "zod";
+
+const schema = z.object({
+    name: z.string().min(3),
+    age: z.number().min(18),
+});
+
+type FormData = z.infer<typeof schema>;
 
 function Form() {
-  const { register, handleSubmit } = useForm()
-  const onSubmit = (data: FieldValues) => console.log(data)
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm();
+    const onSubmit = () => console.log(errors);
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className="my-5">
-      <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name
-        </label>
-        <input
-          {...register("name")}
-          id="name"
-          type="text"
-          className="form-control"
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="age" className="form-label">
-          Age
-        </label>
-        <input
-          {...register("age")}
-          id="age"
-          type="number"
-          className="form-control"
-        />
-      </div>
-      <button className="btn btn-primary">Submit</button>
-    </form>
-  )
+    return (
+        <form className="my-5" onSubmit={handleSubmit(onSubmit)}>
+            <label className="form-label" htmlFor="name">
+                Name
+            </label>
+            <div className="mb-3">
+                <input
+                    className={errors.name ? "form-control is-invalid" : "form-control"}
+                    id="name"
+                    {...register("name", {required: true, minLength: 4})}
+                />
+                {errors.name?.type === "required" && <p className="text-danger">Name is required</p>}
+                {errors.name?.type === "minLength" && <p className="text-danger">Name must be 4 characters long.</p>}
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    );
 }
 
-export default Form
+export default Form;
